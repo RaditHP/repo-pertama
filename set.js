96122f13cfc091f29d8b7512445c2f20a0987283
Rakha HP <rpramudito@gmail.com>
2022-05-28 11:45:14 +0700

    ini adalah test struktur datra set

---
+++ set.js
@@ -0,0 +1,10 @@
+let set = new Set();
+
+let muridPertama = { nama: "mari" };
+let muridKedua = { nama: "Regal" };
+
+set.add(muridPertama);
+set.add(muridKedua);
+set.add(muridPertama);
+
+console.log(set.size);
